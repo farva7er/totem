c3791e1332c49ea5c505c06264a6a55cd171bc85
Nicolai Scurtu <farvater@farvater.xyz>
2022-03-25 21:46:46 +0200

    Added filename and line number to logs

--- totem/src/Log.cpp
+++ totem/src/Log.cpp
@@ -1,23 +1,25 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <ctime>
-#include "Log.h"
 #include <iostream>
 
-void printLog(LogLevel level, const char *projectName, const char *fmt, ...)
+#include "Log.h"
+
+void printLog(LogLevel level, const char* filename, int line,
+               const char *projectName, const char *fmt, ...)
 {
    va_list vl;
    std::tm curr_time;
    time_t time_since_epoch = time(nullptr);
    curr_time = *(localtime(&time_since_epoch));
    printf("%02d:%02d ", curr_time.tm_hour, curr_time.tm_min);
-
-   switch(level) {
+   switch(level)
+   {
       case LogLevel::Info: printf("[Info] "); break;
       case LogLevel::Error: printf("[Error] "); break;
       case LogLevel::Fatal: printf("[FATAL] "); break;
    }
-   printf("%s: ", projectName);
+   printf("%s (%s:%d) ", projectName, filename, line);
    va_start(vl, fmt);
    vprintf(fmt, vl);
    va_end(vl);
--- totem/src/Log.h
+++ totem/src/Log.h
@@ -4,38 +4,53 @@
 #define STRINGIFY(x) #x
 #define TOSTRING(x) STRINGIFY(x)
 
+#include <string.h>
+#define __FILENAME__ \
+   (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+
+
 enum class LogLevel { Info, Error, Fatal };
 
 #ifdef TOTEM_DEBUG
    #ifdef PROJECT_NAME
       #define LOG_INFO(...) \
-               printLog(LogLevel::Info, TOSTRING(PROJECT_NAME), __VA_ARGS__)
+               printLog(LogLevel::Info, __FILENAME__, __LINE__, \
+                        TOSTRING(PROJECT_NAME), __VA_ARGS__)
       #define LOG_ERROR(...) \
-               printLog(LogLevel::Error, TOSTRING(PROJECT_NAME), __VA_ARGS__)
+               printLog(LogLevel::Error, __FILENAME__, __LINE__, \
+                        TOSTRING(PROJECT_NAME), __VA_ARGS__)
       #define LOG_FATAL(...) \
-               printLog(LogLevel::Fatal, TOSTRING(PROJECT_NAME), __VA_ARGS__)
+               printLog(LogLevel::Fatal, __FILENAME__, __LINE__, \
+                        TOSTRING(PROJECT_NAME), __VA_ARGS__)
    #else
       #define LOG_INFO(...) \
-               printLog(LogLevel::Info, "Client", __VA_ARGS__)
+               printLog(LogLevel::Info, __FILENAME__, __LINE__, \
+                     "Client", __VA_ARGS__)
       #define LOG_ERROR(...) \
-               printLog(LogLevel::Error, "Client", __VA_ARGS__)
+               printLog(LogLevel::Error, __FILENAME__, __LINE__, \
+                     "Client", __VA_ARGS__)
       #define LOG_FATAL(...) \
-               printLog(LogLevel::Fatal, "Client", __VA_ARGS__)
+               printLog(LogLevel::Fatal, __FILENAME__, __LINE__, \
+                     "Client", __VA_ARGS__)
    #endif
 #else
    #ifdef PROJECT_NAME
       #define LOG_INFO(...)
       #define LOG_ERROR(...) 
       #define LOG_FATAL(...) \
-               printLog(LogLevel::Fatal, TOSTRING(PROJECT_NAME), __VA_ARGS__)
+               printLog(LogLevel::Fatal, __FILENAME__, __LINE__, \
+                        TOSTRING(PROJECT_NAME), __VA_ARGS__)
    #else
       #define LOG_INFO(...)
       #define LOG_ERROR(...)
       #define LOG_FATAL(...) \
-               printLog(LogLevel::Fatal, "Client", __VA_ARGS__)
+               printLog(LogLevel::Fatal, __FILENAME__, __LINE__, \
+                     "Client", __VA_ARGS__)
+
    #endif
 #endif
 
-void printLog(LogLevel level, const char *projectName, const char *fmt, ...);
+void printLog(LogLevel level, const char* filename, int line,
+               const char *projectName, const char *fmt, ...);
 
 #endif
